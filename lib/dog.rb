3bcbcc259f793adcc23b702e3d45791dd19598e6
Rabah <rmerzoug471@gmail.com>
2022-08-05 22:18:47 -0500

    commit tree 31542359624e2ff01a89d92c7917b5f8d0dbd66e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+    def name=(name)
+        @name = name
+    end
+    def name 
+        @name
+    end
+    def breed=(breede)
+        @breed = breede
+    end
+    def breed 
+        @breed
+    end
 end
